3e903793dfcd63815341e0dda1091d3ca27adecb
Illia Teliuk <illiateliuk@gmail.com>
2024-07-01 11:47:55 +0300

    App styling

--- todolist-client/src/App.tsx
+++ todolist-client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import LoginForm from "./components/auth/LoginForm";
 
 function App() {
-  return <div className="App">ToDo</div>;
+  return <LoginForm />;
 }
 
 export default App;
