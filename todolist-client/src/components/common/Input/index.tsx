1260fd0a93042ac561f3d45c49a397d3a15bf27a
Illia Teliuk <illiateliuk@gmail.com>
2024-07-03 08:46:18 +0300

    Input update

--- todolist-client/src/components/common/Input/index.tsx
+++ todolist-client/src/components/common/Input/index.tsx
@@ -3,19 +3,27 @@ import { InputProps } from "./types";
 import { FC } from "react";
 
 const Input: FC<InputProps> = (props: InputProps) => {
-  const { autoFocus, type, placeholder, fieldName, onChange, error, touched } =
-    props;
+  const {
+    autoFocus,
+    type,
+    placeholder,
+    fieldName,
+    onChange,
+    onBlur,
+    error,
+    touched
+  } = props;
   console.log("Props: ", props);
   return (
     <>
       <TextField
         name={fieldName ? fieldName : ""}
         onChange={onChange}
+        onBlur={onBlur}
         label={placeholder}
         autoFocus={autoFocus}
         type={type}
         placeholder={placeholder}
-        // {...field}
       />
       <Typography color="error">
         {error && touched ? error : "\u00A0"}
